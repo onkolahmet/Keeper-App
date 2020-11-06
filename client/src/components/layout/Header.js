b3dcbf58aaf2bcb38d28e527509fbce6602e0be5
Ahmet Önkol <onkolahmet35@gmail.com>
2020-11-06 19:33:22 +0300

    header file

---
+++ client/src/components/layout/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon /> Keeper
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
